33d3a9f131b035226ffdbfe5f93fbacf2bfb9971
DABWLearning <141861165+DABWLearning@users.noreply.github.com>
2023-09-02 09:16:25 -0600

    Update streamlit_app.py
    normalization and displayed as table

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,5 +24,11 @@ streamlit.dataframe(fruits_to_show)
 
 import requests
 streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+#streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
+# normalized json
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# table output
+streamlit.dataframe(fruityvice_normalized)
+
