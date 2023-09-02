183d98a98c465765f6c411d40138c606c52f3723
DABWLearning <141861165+DABWLearning@users.noreply.github.com>
2023-09-02 13:46:05 -0600

    Update streamlit_app.py
    connection issue

--- streamlit_app.py
+++ streamlit_app.py
@@ -76,7 +76,7 @@ my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
-my_cnx.close()
+#my_cnx.close()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
 
@@ -100,7 +100,7 @@ def get_fruit_load_list():
 if streamlit.button('Get Fruit Load List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()
-    my_cnx.close()
+    #my_cnx.close()
     streamlit.dataframe(my_data_row)
 
 def insert_row_snowflake(new_fruit):
@@ -108,7 +108,10 @@ def insert_row_snowflake(new_fruit):
         #my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values ('from streamlit')")
         my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values ('"+new_fruit+"')")
         return 'Thanks for adding ' + new_fruit
+
+
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')      
+
 if streamlit.button('Add a Fruit to the List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     back_from_function = insert_row_snowflake(add_my_fruit)
